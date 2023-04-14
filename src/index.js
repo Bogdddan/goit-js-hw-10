a53ec34fb27fe9fad8816dca0601f96308b93eca
Bogdddan <bogdankrajnik95@gmail.com>
2023-04-14 22:18:27 +0300

    hp

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 const input = document.getElementById('search-box');
+const div = document.querySelector('.country-info');
 
 // ставим затримку 300мс щоб часто не оновлювався інпут
 const debounceSearch = debounce(fetchCountries , DEBOUNCE_DELAY);
@@ -15,64 +16,57 @@ const ul = document.querySelector('.country-list');
 function fetchCountries() {
 const handleSearch = input.value.trim();
 
-if (handleSearch !== '') {
+if (handleSearch !== ' ') {
     return fetch(`https://restcountries.com/v3.1/name/${handleSearch} `)
     .then(response => {
         return response.json();
     })
     .then(countries => {
         console.log(countries);
-        const div = document.querySelector('.country-info');
-        const capital = countries.map(country => country.capital);
-        const population = countries.map(country => country.population);
-        const languages = countries.map(country => country.languages).flat();
-        const countryName = countries.map(country => country.name.official);
-        const countryFlag = countries.map(country => country.flags.png);
-        ul.innerHTML = `<p><img src=${countryFlag} class="photo"/>${countryName}</p>`;
-        div.innerHTML = `
-            <p><div class="infor">Capital</div> ${capital}</p>
-            <p><div class="infor">Population</div> ${population}</p>
-            <p><div class="infor">Languages</div> ${languages}</p>
-        `;
+        renderPage(countries);
         })
         .catch(error => {
-            console.log(error)
+            console.log(error);
+            Notiflix.Notify.failure('Oops, there is no country with that name');
+            ul.innerHTML = '';
+            div.innerHTML = '';
         });
     } else {
         ul.innerHTML = '';
 }
 }
 
+function renderPage(countries) {
+    const countryCount = countries.length;
 
-
+    if (countryCount > 10) {
+        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+        ul.innerHTML = '';
+        div.innerHTML = '';
+    } else if (countryCount > 1) {
+    const countryList = countries
+        .map(country => {
+            const { name: { common }, flags: { svg } } = country;
+            return `<li class="country-item"><img src="${svg}" alt="${common}" class="flag-image">${common}</li>`;
+        })
+        .join('');
+        ul.innerHTML = countryList;
+        div.innerHTML = '';
+        } else {
+        const [country] = countries;
+        const { name: { common }, capital, population, languages, flags: { svg } } = country;
+        ul.innerHTML = `<p class="country-name"><img src="${svg}" class="photo"/>${common}</p>`;
+        div.innerHTML = `
+            <p><div class="infor">Capital :</div> ${capital ?? 'N/A'}</p>
+            <p><div class="infor">Population :</div> ${population ?? 'N/A'}</p>
+            <p><div class="infor">Languages :</div> ${Object.values(languages ?? {}).join(', ') || 'N/A'}</p>
+        `;
+        }
+        
+    }
+//      
 // 
 // 
 // 
 // 
-// 
-function errorFunc() {
-    return Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-};
 
-// fetch(`https://restcountries.com/v3.1/name/deutschland`).then(r => {
-//     return r.json()
-// }).then(countries => {
-//     console.log(countries)
-// }).catch(error => {
-//     console.log(error)
-// })
-
-//.map(return `
-//           <p>Capital {{capital}}</p>
-//               <p>Population {{population}}</p>
-//                   <p>Languages {{languages}}</p>
-//`)
-
-// const searchText = input.value.trim(); // Санітизація введеного рядка з полі пошуку
-// if (searchText === '') {
-//   // Виконується якщо поле пошуку порожнє після санітизації
-//   // Здесь можна додати код для очищення списку країн або інформації про країну
-// } else {
-//   // Виконується якщо в полі пошуку є текст після санітизації
-//   // Здесь можна додати код для виконання HTTP-запиту та відображення результатів
-// }
